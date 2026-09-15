c8b46a8e5a03e979f4e55cabde0a01e03c24236f
kzawadzkii656 <126247389+kzawadzkii656@users.noreply.github.com>
2026-09-15 23:02:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='gielda-v7-final-1',ASSETS=['./','./index.html','./404.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+const C='gielda-v7-9',ASSETS=['./','./index.html','./404.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{let q=r.clone();caches.open(C).then(c=>c.put('./index.html',q));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{let q=n.clone();caches.open(C).then(c=>c.put(e.request,q));return n})))});
